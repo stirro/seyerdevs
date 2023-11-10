065c4b154dfb0fbfd2681d5b23a76daa0996cf9a
stirro <122248916+stirro@users.noreply.github.com>
2023-11-10 12:16:23 -0600

    modificacion del background del banner

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,28 +28,20 @@ export const Projects = () => {
 
     return (
     <section className="projects" id="project">
+        <h1>PROYECTOS</h1>
         <Container>
-        <h1>DEVELOPING</h1>
         <Row>
             <Col>
             <div className="projects-desc">
-            <h2>Creando soluciones para todos<span className></span></h2>
-                <p>Hola Soy Sergio Tirro!, tengo 24 años, soy Técnico en informática y estudio la carrera de 
-                Ing. en Comunicaciones y Electrónica en el IPN. Con más de 4 años de experiencia en el área de sistemas computacionales tengo habilidades
-                para el desarrollo de soluciones, desarrollo de proyectos, liderazgo y paciencia y amor por mi profesión.
+            <h2>Creando soluciones para todos.<span className></span></h2>
+                <p>Durante mi vida profesional he tenido la oportunidad de trabajar en diferentes proyectos, 
+                    los cuales me han permitido desarrollar habilidades y conocimientos en diferentes áreas de la tecnología en las 
+                    áreas de la programación, diseño y administración de proyectos, redes y muchas más.
                 </p>
             </div>
             <div className="projects-box1">
                 <Carousel responsive={responsive} infinite={true} className="projects-slider">
                     <div className="item1">
-                        <img src={skill_1} alt="c++"/>
-                        <h5>televisa.</h5>
-                    </div>
-                </Carousel>
-            </div>
-            <div className="projects-box2">
-                <Carousel responsive={responsive} infinite={true} className="projects-slider">
-                    <div className="item2">
                         <img src={skill_1} alt="c++"/>
                         <h5>televisa.</h5>
                     </div>
